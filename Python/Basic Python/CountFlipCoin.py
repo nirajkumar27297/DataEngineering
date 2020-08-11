80c6f162658650860ea4cc998e2487da3f075ca9
nirajkumar27297 <60932846+nirajkumar27297@users.noreply.github.com>
2020-08-11 13:04:24 +0530

    Added:Python codes for counting number of flips of coin

---
+++ Python/Basic Python/CountFlipCoin.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 20:29:38 2020
+
+@author: Niraj
+Count Percentage of Heads and Tails
+"""
+import random 
+
+try:
+    numberOfFlips = int(input("Enter the number of times you want to flip the coin "))
+    if( numberOfFlips < 0 ):
+        raise Exception
+    #Total count
+    totalCount = 0
+    countHead = 0
+    while( totalCount < numberOfFlips ):
+        flip = random.random()
+        if ( flip > 0.5 ):
+            countHead += 1
+        totalCount += 1
+    headPercentage = (countHead / totalCount) * 100
+    print("Percentage of Head is {0:.2f}".format(headPercentage))
+    tailPercentage = ((totalCount-countHead) / totalCount) * 100
+    print("Percentage of Tail is {0:.2f}".format(tailPercentage))
+
+except(Exception):
+    print("Enter a positive number")
+    
+    
+
+
